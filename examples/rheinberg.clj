460b504d9ec1e106b2e53f8abbb01584e0915107
Ludger Solbach <soulman@soulspace.org>
2023-03-21 18:36:32 +0100

    enhanced comments

--- examples/rheinberg.clj
+++ examples/rheinberg.clj
@@ -1,19 +1,19 @@
-;;
-;;   Copyright (c) Ludger Solbach. All rights reserved.
-;;   The use and distribution terms for this software are covered by the
-;;   Eclipse Public License 1.0 (http://opensource.org/licenses/eclipse-1.0.php)
-;;   which can be found in the file license.txt at the root of this distribution.
-;;   By using this software in any fashion, you are agreeing to be bound by
-;;   the terms of this license.
-;;   You must not remove this notice, or any other, from this software.
-;;
-
-;; Example for using the SVG DSL 
+;;;;
+;;;;   Copyright (c) Ludger Solbach. All rights reserved.
+;;;;
+;;;;   The use and distribution terms for this software are covered by the
+;;;;   Eclipse Public License 1.0 (http://opensource.org/licenses/eclipse-1.0.php)
+;;;;   which can be found in the file license.txt at the root of this distribution.
+;;;;   By using this software in any fashion, you are agreeing to be bound by
+;;;;   the terms of this license.
+;;;;
+;;;;   You must not remove this notice, or any other, from this software.
+;;;;
 
 (ns rheinberg
+  "Create Rheinberg and darkfield filters for microscopy.b"
   (:require [org.soulspace.xml.dsl.svg-dsl :as svg]
             [clojure.data.xml :as xml]))
-
 
 (defn dual-filter
   "Generates a filter with an inner and an outer circle."
